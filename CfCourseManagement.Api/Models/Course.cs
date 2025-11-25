7939051201ebb5c80f22ca147e8aba7bfa771311
kolio <odysseas.kolios.com>
2025-11-25 19:33:38 +0200

    feat: add Course model and EF Core database setup

---
+++ CfCourseManagement.Api/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace CfCourseManagement.Api.Models
+{
+    public class Course
+    {
+        public int Id { get; set; } // Πρωτεύον κλειδί (PK) στη βάση
+        public string Title { get; set; } // Τίτλος μαθήματος (υποχρεωτικό)
+        public string? Description { get; set; } // Περιγραφή (μπορεί να είναι κενό -> ?)
+        public int Credits { get; set; }  // Π.χ. ώρες / μονάδες μαθήματος
+        public DateTime StartDate  { get; set; }  // Ημερομηνία έναρξης
+        public DateTime EndDate { get; set; } // Ημερομηνία λήξης
+    }
+}
--- CfCourseManagement.Api/Program.cs
+++ CfCourseManagement.Api/Program.cs
@@ -1,3 +1,5 @@
+﻿using CfCourseManagement.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CfCourseManagement.Api
 {
@@ -8,13 +10,17 @@ namespace CfCourseManagement.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            // Εδώ προσθέτουμε τον ApplicationDbContext και τον συνδέουμε με SQL Server
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
